b1fa0480814c7bbf9ddf9188e64bf9dbdb40e465
Priyanshu Vishwakarma <71216498+priyanshu091@users.noreply.github.com>
2026-03-15 22:11:00 +0530

    Add files via upload

---
+++ MultiThreading/Synchronized/Counter.java
@@ -0,0 +1,15 @@
+package MultiThreading.Synchronized;
+
+public class Counter {
+
+    private int count =0;
+
+    public synchronized void  increment(){
+        count++;
+    }
+    public int getCount(){
+        return count;
+    }
+
+
+}
---
+++ MultiThreading/Synchronized/MyThread.java
@@ -0,0 +1,20 @@
+package MultiThreading.Synchronized;
+
+public class MyThread extends Thread{
+
+    private Counter counter;
+
+    MyThread(Counter counter){
+
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 1000; i++) {
+
+        counter.increment();
+        }
+    }
+}
